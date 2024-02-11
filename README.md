735efd26590ab5a86bd3f0205276660fea785dbb
Mark Lee <leemark@gmail.com>
2024-02-10 20:21:00 -0700

    requirements

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LangGraph
 hello world
+
+```
+pip install langchain langgraph langchain-openai langsmith langchainhub duckduckgo-search beautifulsoup4 gradio python-dotenv
+```
--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 # conda activate langgraph-start
-# 
+# pip install langchain langgraph langchain-openai langsmith langchainhub duckduckgo-search beautifulsoup4 gradio python-dotenv
+
